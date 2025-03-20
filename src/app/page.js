d31b34d9ab8b12e2a07c7e919bed0fe02e7b4e89
Vaishnavi Patil <vaishnavip276@gmail.com>
2025-03-21 05:05:09 +0530

    button

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 // File: app/page.js
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
   const components = [
     {
-      title: 'Parallex scroll Slider',
-      path: '/lenis-scroll',
-      experimenting: false
+      title: "Parallex scroll Slider",
+      path: "/lenis-scroll",
+      experimenting: false,
     },
   ];
 
@@ -15,30 +15,25 @@ export default function Home() {
       <h1 className="text-7xl font-black mb-20 tracking-tight">
         Frames by Vaishhh🌻
       </h1>
-      
+
       <div className="grid grid-cols-1 md:grid-cols-2 gap-x-12 gap-y-4">
         <div className="space-y-4">
           {components.slice(0, 9).map((component) => (
             <div key={component.path} className="flex items-center">
-              <Link 
+              <Link
                 href={component.path}
-                className="text-black hover:underline text-xl underline tracking-tighter font-medium"
+                className="text-white text-md  font-medium bg-slate-900 hover:bg-slate-900/80 px-4 py-2 rounded-sm"
               >
                 {component.title}
               </Link>
-              {component.experimenting && (
-                <span className="ml-2 text-xs px-2 py-1 bg-orange-100 text-orange-700 rounded-md">
-                  still experimenting
-                </span>
-              )}
             </div>
           ))}
         </div>
-        
+
         <div className="space-y-4">
           {components.slice(9).map((component) => (
             <div key={component.path} className="flex items-center">
-              <Link 
+              <Link
                 href={component.path}
                 className="text-black hover:underline text-xl underline tracking-tighter font-medium"
               >
